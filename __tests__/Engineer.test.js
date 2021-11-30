5f95ba4e555b53168dfa4149fa36090e4427463f
davezer <davezer27@gmail.com>
2021-11-30 15:23:51 -0500

    added Engineer constructor with passing tests

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,16 @@
+const Engineer = require('../lib/Engineer');
+
+test('sets gitHub username using constructor', () => {
+    const gitHub = 'davezer';
+    const employee = new Engineer('Dave', '100', 'daveoliverio@gmail.com', gitHub);
+    expect(employee.gitHub).toBe(gitHub);
+
+});
+
+test('getRole() returns the Engineer role to employee', () => {
+    const role = 'Engineer';
+    const employee = new Engineer('Dave', '100', 'daveoliverio@gmail.com', 'davezer');
+
+    expect(employee.getRole()).toBe(role);
+
+})
