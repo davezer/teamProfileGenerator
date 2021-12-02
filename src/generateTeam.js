f2478d37221212840343a8841bfda41c8988f825
davezer <davezer27@gmail.com>
2021-12-02 12:57:31 -0500

    added functions to dynamically create the html

--- src/generateTeam.js
+++ src/generateTeam.js
@@ -28,20 +28,82 @@ function generateCards(team) {
 }
 
 let generateManagerCard = (Manager) => {
-    return ``
+    return `
+    <div class='card m-1 shadow' style='width: 18rem'>
+    <div class='card-header'>
+        <h3 class='card-title'>${Manager.getName()}</h3>
+        <h6 class='card-text'>${Manager.getRole()}</h6>
+    </div>
+    <div class='card-body'>
+        <ul class='list-group list-group-flush'>
+            <li class='list-group-item'>ID: ${Manager.getId()}</li>
+            <li class='list-group-item'>Email: ${Manager.getEmail()}</li>
+            <li class='list-group-item'>School: ${Manager.getOfficeNumber()}</li>
+        </ul>
+    </div>
+</div>
+    `
 }
 
 let generateEngineerCard = (Engineer) => {
-    return ``
+    return `
+    <div class='card m-1 shadow' style='width: 18rem'>
+    <div class='card-header'>
+        <h3 class='card-title'>${Engineer.getName()}</h3>
+        <h6 class='card-text'>${Engineer.getRole()}</h6>
+    </div>
+    <div class='card-body'>
+        <ul class='list-group list-group-flush'>
+            <li class='list-group-item'>ID: ${Engineer.getId()}</li>
+            <li class='list-group-item'>Email: ${Engineer.getEmail()}</li>
+            <li class='list-group-item'>School: ${Engineer.getGitHub()}</li>
+        </ul>
+    </div>
+</div>
+    `
 }
 
 let generateInternCard = (Intern) => {
-    return ``
+    return `
+    <div class='card m-1 shadow' style='width: 18rem'>
+        <div class='card-header'>
+            <h3 class='card-title'>${Intern.getName()}</h3>
+            <h6 class='card-text'>${Intern.getRole()}</h6>
+        </div>
+        <div class='card-body'>
+            <ul class='list-group list-group-flush'>
+                <li class='list-group-item'>ID: ${Intern.getId()}</li>
+                <li class='list-group-item'>Email: ${Intern.getEmail()}</li>
+                <li class='list-group-item'>School: ${Intern.getSchool()}</li>
+            </ul>
+        </div>
+    </div>
+    `
 }
 
 function generateTeam(team) {
     console.log(team);
-    return ``
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+      <title>My Team</title>
+    </head>
+    <body>
+    <div class="jumbotron jumbotron-fluid bg-warning">
+      <div class="container">
+        <h1 class="display-4 text-center">My Team</h1>
+      </div>
+    </div>
+    <div class="d-flex flex-row flex-wrap justify-content-center">
+    ${generateCards(team)}
+    </div>
+    </body>
+    </html>`
 }
 
 module.exports = generateTeam;
